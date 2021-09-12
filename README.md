a10bc6cb320a06adc40aab5988fd31df78d5c66a
Tyler Saxton <tyler.l.saxton@gmail.com>
2021-09-12 18:53:27 +1000

    Create process to identify intersections in OSM (including at the margins) and generate list of intersection coordinates with optional 10m intervals

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # minor_thesis
 Code used for Minor Thesis research
+
+## Jupyter Notebooks
+
+GPU_test.ipynb = train a simple TensorFlow 2 model to recognise characters (MNIST)
+as a demo/test to ensure GPU is being utilized in the environment
+
+Google_Streetview_API.ipynb = Demonstrate how to download a GSV image for a
+co-ordinate (lat/lon) and how to calculate a bearing from one co-ordinate to the next
+
+PBN_Geocoding.ipynb = Use local Nominatim server to perform reverse-geocoding
+of streets in PBN geojson file.  Superceded by Find_Intersections.ipynb
+and eventually PBN_Distinct_Intersections.ipynb.
+
+Find_Intersections.ipynb = Original notebook that uses an OSM extract of Victoria +
+a local Nominatim server + the CSV data extracted from the PBN .geojson file
+to reverse-geocode all "existing" routes in PBN, then find the intersections
+
+Points_and_Bearings.ipynb = Demo of how to calculate the bearing from one co-ordinate
+to the next, AND to expand the co-ordinate pair into a a list of co-ordinates
+at 10m intervals
+
+### PBN_Distinct_Intersections.ipynb
+Load PBN geojson file + OSM extract of Victoria,
+use local Nominatim serve to perform reverse-geocoding, use OSM to find the
+intersections within the bounding box of the original street, find the bearings,
+save as CSV for use in GSV_Demo.ipynb.  Appears to supercede Find_Intersections.ipynb.
+
+Google_Streetview_API.ipynb = Demonstrate how to download a GSV image for a
+co-ordinate (lat/lon) and how to calculate a bearing from one co-ordinate to the next
+
+GSV_Demo.ipynb = given a list of intersection co-ordinates along "EXISTING" PBN routes 
+and the bearing at each co-ordinate, sample one co-ordinate at a time, allow manual
+specification of metres forward/backward from the intersection along the bearing and
+manual override of the bearing, then download and display 4x GSV images at 0/90/180/270
+from the bearing.  Allow "hits" (where a bicycle lane logo etc. is found in an image)
+to be recorded in a CSV "hits.csv" for later labelling.
+
+### Filter_PBN_Existing.ipynb
+Take the original PBN .geojson file and filter it
+into both "EXISTING" vs not "EXISTING" (planned) so that we can draw them
+as differenct colours in Map_GEOJSON.ipynb
+
+### OSM_Filter.ipynb
+Extract the "shape" of a "Locality" from a govt .geojson file,
+and then save it to a new .geojson file.  Includes instructions to then use "osmium"
+to filter an OSM extract down to just that locality, and save as an OXM file (XML)
+
+### Parse_OSM.ipynb
+Load an OSM file into memory/dict objects, find the ways that
+have a "cycleway" tag, then save out as a .geojson file for drawing on a map
+
+### Map_GEOJSON.ipynb
+Given .geojson files that show bicycle networks as lines from
+one co-ordinate (lat/lon) to the next, draw them on a map for comparison
+
+### Parse_GeoJSON.ipynb
+Read a version of OSM cycleway or road data that has been converted into a .geojson
+via Parse_OSM.ipynb.  Turn it into a list of co-ordinate pairs along each way,
+and then expand those co-ordinate pairs into a list of co-ordinates at 10m intervals
+=> In "Mount Eliza" there are 1804 "ways" (road segments) and 75740 points if we
+sample at 10m intervals.  At $7 USD per 1000 images and 4 images per point, this
+would be $303 USD for the one town.
+
+
+## Python Classes
+
+osm_filter.py = Called by OSM_Filter.ipynb to load a "locality" geojson file and then
+filter it down to only one feature that matches the "vic_loca_2" property, then
+save it as a smaller geosjon file representing the "shape" of the selected locality
+
--- osm_gsv_utils/osm_filter.py
+++ osm_gsv_utils/osm_filter.py
@@ -7,6 +7,9 @@ Created on 06 Sep 2021
 import geojson
 from geojson import Feature, FeatureCollection, dump
 
+import geopy
+import geopy.distance
+
 
 class osm_filter(object):
     '''
@@ -26,7 +29,7 @@ class osm_filter(object):
 
         json_file.close()
 
-    def save_locality(self, filename, filter_value, filter_key='vic_loca_2'):
+    def save_locality(self, locality, filename_shape, filter_value, filter_key='vic_loca_2', margin=200):
         # Find the locality
         gj_index    = 0
         gj_selected = 0
@@ -47,11 +50,69 @@ class osm_filter(object):
         if gj_selected == 0:
             raise Exception("Could not find locality")
 
+        # Find track of the overall bounding box
+        lat_min =  999
+        lat_max = -999
+        lon_min =  999
+        lon_max = -999
+
+        geometry    = gj_selected['geometry']
+        coordinates = geometry['coordinates']
+
+        flat_coordinates = self.flatten_coordinates(coordinates)
+
+        for point in flat_coordinates:
+            lat_point = point[1]
+            lon_point = point[0]
+
+            if lon_point < lon_min:
+                lon_min = lon_point
+            if lon_point > lon_max:
+                lon_max = lon_point
+            if lat_point < lat_min:
+                lat_min = lat_point
+            if lat_point > lat_max:
+                lat_max = lat_point
+
+        print('Bounding box excl margin: {0:.6f}, {1:.6f} => {2:.6f}, {3:.6f}'.format(lat_min, lon_min, lat_max, lon_max));
+
+        origin_min = geopy.Point(lat_min, lon_min)
+        margin_min = geopy.distance.distance(meters=margin).destination(point=origin_min, bearing=225) # SW
+
+        origin_max = geopy.Point(lat_max, lon_max)
+        margin_max = geopy.distance.distance(meters=margin).destination(point=origin_max, bearing=45)  # NE
+
+        print('Bounding box with margin: {0:.6f}, {1:.6f} => {2:.6f}, {3:.6f}'.format(margin_min.latitude, margin_min.longitude, margin_max.latitude, margin_max.longitude))
+
         # Save the locality to a new file
         features = []
         features.append(gj_selected)
         feature_collection = FeatureCollection(features)
 
-        with open(filename, 'w') as output_file:
-            dump(feature_collection, output_file)
+        with open(filename_shape, 'w') as output_file_shape:
+            dump(feature_collection, output_file_shape)
 
+        print('\nRun the following two osmium commands:\n')
+
+        print('First command:  Extract OSM data according to the official "shape" of the Locality\n')
+
+        print('osmium extract --polygon=Locality_' + locality + '.geojson australia-latest.osm.pbf -o Locality_' + locality + '.osm')
+
+        print('\nSecond command:  Extract OSM data for a bounding box ' + str(margin) + ' meters bigger than the "shape"')
+        print('This second extract will be used to ensure we do not miss any intersections with streets that are JUST outside the locality\n')
+
+        print('osmium extract --bbox=' + str(margin_min.longitude) + ',' + str(margin_min.latitude) + ',' + str(margin_max.longitude) + ',' + str(margin_max.latitude) +
+            ' australia-latest.osm.pbf -o Locality_' + locality + '_margin.osm')
+
+    def flatten_coordinates(self, coordinates):
+        coordinates_out = []
+
+        if type(coordinates[0]) is list:
+            if type(coordinates[0][0]) is list:
+                for item in coordinates:
+                    coordinates_out = coordinates_out + self.flatten_coordinates(item)
+                return coordinates_out
+            else:
+                return coordinates
+        else:
+            return coordinates
